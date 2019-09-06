a66c98b88aff0cec5ad65128098244940a8e11af
zhaofeng-shu33 <616545598@qq.com>
2019-09-06 19:56:53 +0800

    use script files

--- themes/navigator-hugo/assets/js/script.js
+++ themes/navigator-hugo/assets/js/script.js
@@ -219,59 +219,106 @@
 
 
 window.marker = null;
-
+<script type="text/javascript">
+    //创建和初始化地图函数：
+    function initMap(longitude, latitude){
+        createMap(longitude, latitude);//创建地图
+        setMapEvent();//设置地图事件
+        addMapControl();//向地图添加控件
+        addMarker();//向地图中添加marker
+    }
+    
+    //创建地图函数：
+    function createMap(longitude, latitude){
+        var map = new BMap.Map("map");//在百度地图容器中创建一个地图
+        var point = new BMap.Point(longitude, latitude);//定义一个中心点坐标, e.g. (114.01158,22.601709)
+        map.centerAndZoom(point,18);//设定地图的中心点和坐标并将地图显示在地图容器中
+        window.map = map;//将map变量存储在全局
+    }
+    
+    //地图事件设置函数：
+    function setMapEvent(){
+        map.enableScrollWheelZoom();//启用地图滚轮放大缩小
+        map.enableKeyboard();//启用键盘上下左右键移动地图
+    }
+    
+    //地图控件添加函数：
+    function addMapControl(){
+        //向地图中添加缩放控件
+	var ctrl_nav = new BMap.NavigationControl({anchor:BMAP_ANCHOR_TOP_LEFT,type:BMAP_NAVIGATION_CONTROL_LARGE});
+	map.addControl(ctrl_nav);
+        //向地图中添加缩略图控件
+	var ctrl_ove = new BMap.OverviewMapControl({anchor:BMAP_ANCHOR_BOTTOM_RIGHT,isOpen:1});
+	map.addControl(ctrl_ove);
+        //向地图中添加比例尺控件
+	var ctrl_sca = new BMap.ScaleControl({anchor:BMAP_ANCHOR_BOTTOM_LEFT});
+	map.addControl(ctrl_sca);
+    }
+    
+    //标注点数组
+    var markerArr = [{title:"4I&nbsp;Lab",content:"我的备注",point:"114.011513|22.601976",isOpen:0,icon:{w:21,h:21,l:0,t:0,x:6,lb:5}}
+		 ];
+    //创建marker
+    function addMarker(){
+        for(var i=0;i<markerArr.length;i++){
+            var json = markerArr[i];
+            var p0 = json.point.split("|")[0];
+            var p1 = json.point.split("|")[1];
+            var point = new BMap.Point(p0,p1);
+			var iconImg = createIcon(json.icon);
+            var marker = new BMap.Marker(point,{icon:iconImg});
+			var iw = createInfoWindow(i);
+			var label = new BMap.Label(json.title,{"offset":new BMap.Size(json.icon.lb-json.icon.x+10,-20)});
+			marker.setLabel(label);
+            map.addOverlay(marker);
+            label.setStyle({
+                        borderColor:"#808080",
+                        color:"#333",
+                        cursor:"pointer"
+            });
+			
+			(function(){
+				var index = i;
+				var _iw = createInfoWindow(i);
+				var _marker = marker;
+				_marker.addEventListener("click",function(){
+				    this.openInfoWindow(_iw);
+			    });
+			    _iw.addEventListener("open",function(){
+				    _marker.getLabel().hide();
+			    })
+			    _iw.addEventListener("close",function(){
+				    _marker.getLabel().show();
+			    })
+				label.addEventListener("click",function(){
+				    _marker.openInfoWindow(_iw);
+			    })
+				if(!!json.isOpen){
+					label.hide();
+					_marker.openInfoWindow(_iw);
+				}
+			})()
+        }
+    }
+    //创建InfoWindow
+    function createInfoWindow(i){
+        var json = markerArr[i];
+        var iw = new BMap.InfoWindow("<b class='iw_poi_title' title='" + json.title + "'>" + json.title + "</b><div class='iw_poi_content'>"+json.content+"</div>");
+        return iw;
+    }
+    //创建一个Icon
+    function createIcon(json){
+        var icon = new BMap.Icon("http://app.baidu.com/map/images/us_mk_icon.png", new BMap.Size(json.w,json.h),{imageOffset: new BMap.Size(-json.l,-json.t),infoWindowOffset:new BMap.Size(json.lb+5,1),offset:new BMap.Size(json.x,json.h)})
+        return icon;
+    }
+    
+    
 function initialize() {
     var map;
 
 	var latitude = $('#map').data('lat');
 	var longitude = $('#map').data('long');
-    var nottingham = new google.maps.LatLng(latitude, longitude);
-
-    var style = [
-    	{"stylers": [{"hue": "#ff61a6"},{"visibility": "on"},{"invert_lightness": true},{"saturation": 40},{"lightness": 10}]}
-	];
-
-    var mapOptions = {
-        // SET THE CENTER
-        center: nottingham,
-
-        // SET THE MAP STYLE & ZOOM LEVEL
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        zoom:9,
-
-        // SET THE BACKGROUND COLOUR
-        backgroundColor:"#000",
-
-        // REMOVE ALL THE CONTROLS EXCEPT ZOOM
-        zoom:17,
-        panControl:false,
-        zoomControl:true,
-        mapTypeControl:false,
-        scaleControl:false,
-        streetViewControl:false,
-        overviewMapControl:false,
-        zoomControlOptions: {
-            style:google.maps.ZoomControlStyle.LARGE
-        }
-
-    }
-    map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-    // SET THE MAP TYPE
-    var mapType = new google.maps.StyledMapType(style, {name:"Grayscale"});
-    map.mapTypes.set('grey', mapType);
-    map.setMapTypeId('grey');
-
-    //CREATE A CUSTOM PIN ICON
-    var marker_image = $('#map').data('marker');
-    var pinIcon = new google.maps.MarkerImage(marker_image,null,null, null,new google.maps.Size(25, 33));
-	
-    marker = new google.maps.Marker({
-        position: nottingham,
-        map: map,
-        icon: pinIcon,
-        title: 'navigator'
-    });
+    initMap(longitude, latitude);//创建和初始化地图
 }
 
 var map = $('#map');
